6948df7527ccbba514f44124bee4266fa11f5e8a
nixo <nicolo@nixo.xyz>
2021-04-25 10:20:52 +0200

    Add Dates formatting support
    Fixes issue #5

--- julia/stdlib.jl
+++ julia/stdlib.jl
@@ -1,4 +1,14 @@
 # Display function for types defined in Julia standard library
+using Dates
+function display(d::ObJuliaDisplay, ::MIME"text/org",
+                 i::Date; kwargs...) where T <: Any
+    print(d.io, Dates.format(i, "[yyyy-mm-dd e]"))
+end
+function display(d::ObJuliaDisplay, ::MIME"text/org",
+                 i::DateTime; kwargs...) where T <: Any
+    print(d.io, Dates.format(i, "[yyyy-mm-dd e HH:MM]"))
+end
+
 function display(d::ObJuliaDisplay, ::MIME"text/org",
                  i::Array{T,2}; kwargs...) where T <: Any
     out = eachrow(i) |> x -> join([join(l, ',') for l in x], '\n')
