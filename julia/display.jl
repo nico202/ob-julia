657daf81f563e4afbfd0dad5c02349731f7dea86
nixo <nicolo@nixo.xyz>
2021-04-25 11:02:01 +0200

    Fallback to text/plain when printing text/org on unknown types

--- julia/display.jl
+++ julia/display.jl
@@ -7,6 +7,9 @@ end
 function display(d::ObJuliaDisplay, Any, x; kwargs...)
     show(d.io, x)
 end
+function display(d::ObJuliaDisplay, ::MIME"text/org", x; kwargs...)
+    show(d.io, MIME("text/plain"), x)
+end
 displayable(d::ObJuliaDisplay, M::MIME) = true
 
 display(d::ObJuliaDisplay, x) = display(d, MIME("text/org"), x)
