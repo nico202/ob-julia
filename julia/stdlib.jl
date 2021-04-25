50dad99cf9a7bed23c9cc6101eefb86bbb4ae9fa
nixo <nicolo@nixo.xyz>
2021-04-25 10:51:34 +0200

    remove duplicate function slipped in

--- julia/stdlib.jl
+++ julia/stdlib.jl
@@ -9,9 +9,3 @@ function display(d::ObJuliaDisplay, ::MIME"text/org",
                  i::DateTime; kwargs...) where T <: Any
     print(d.io, Dates.format(i, "[yyyy-mm-dd e HH:MM]"))
 end
-
-function display(d::ObJuliaDisplay, ::MIME"text/org",
-                 i::Array{T,2}; kwargs...) where T <: Any
-    out = eachrow(i) |> x -> join([join(l, ',') for l in x], '\n')
-    print(d.io, out)
-end
