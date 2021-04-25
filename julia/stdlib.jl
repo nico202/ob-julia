44709efa927021509e38c87f729a73d4ae0c710d
nixo <nicolo@nixo.xyz>
2021-04-25 10:31:01 +0200

    julia: stdlib: Fix formatting

--- julia/stdlib.jl
+++ julia/stdlib.jl
@@ -4,6 +4,7 @@ function display(d::ObJuliaDisplay, ::MIME"text/org",
                  i::Date; kwargs...) where T <: Any
     print(d.io, Dates.format(i, "[yyyy-mm-dd e]"))
 end
+
 function display(d::ObJuliaDisplay, ::MIME"text/org",
                  i::DateTime; kwargs...) where T <: Any
     print(d.io, Dates.format(i, "[yyyy-mm-dd e HH:MM]"))
