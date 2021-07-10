6f36ebbd777b12b5ceebfdcdc4878c206c27cb45
TEC <tec@tecosaur.com>
2021-07-11 03:12:24 +0800

    Use inactive Org timestamp when stringifying Date

--- julia/stdlib.jl
+++ julia/stdlib.jl
@@ -7,6 +7,8 @@ fmt(date::DateTime) = Dates.format(date, "yyyy-mm-dd e HH:MM")
 inactive(d) = string("[", fmt(d), "]")
 inactive(d::String) = string("[", d, "]")
 
+stringify(d::Union{Date,DateTime}) = inactive(d)
+
 display(d::ObJuliaDisplay, ::MIME"text/org", i::T;
         kwargs...) where T <: Union{Date,DateTime} =
             verbatim(d, inactive(i))
